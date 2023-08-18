cfdf426754989fa903cf88299b0e91ed9aef66c0
HeitorDenech <heitor.denech@gmail.com>
2023-08-18 17:50:06 -0300

    segundaAPI

---
+++ src/server.js
@@ -0,0 +1,20 @@
+// importar o pacote do express
+
+const express = require("express");
+
+// instanciar o express na variavel app
+
+const app = express();
+
+// definir porta do servidor em uma variavel
+
+const PORT = 3005;
+
+// testar api com a função listen
+
+app.listen(PORT, () => console.log(`Running at port ${PORT}`))
+
+
+
+
+
